f7b36c13dcf08a0d6347256369c54f6ea6aa662a
Satyam Srivastava <104303280+satya7041@users.noreply.github.com>
2025-05-23 12:20:35 +0000

    rename folder and worked on hashmap

---
+++ prep/hashMap/script.js
@@ -0,0 +1,10 @@
+//In build function of using hashMap
+function runFunction(){
+ const hashMap = new Map();
+ hashMap.set ("h1","Satyam");
+ hashMap.set(2,"Satyam");
+ hashMap.set(212,5656);
+ console.log("hello",hashMap.get("h1"));
+ console.log("her",hashMap.get(212));
+ console.log(hashMap.get(2));
+};
