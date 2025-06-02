d805325e664d7d91c4f0319ba7fb0bcb65178ab3
Satyam Srivastava <104303280+satya7041@users.noreply.github.com>
2025-06-02 08:00:52 +0000

    strings function

---
+++ prep/strings/script.js
@@ -0,0 +1,41 @@
+// Check the two strings are anagram or not
+
+// function runFunction(){
+//   console.log("Hello");
+
+//   const str1 = document.getElementById("str1").value;
+//   const str2 = document.getElementById("str2").value;
+
+//   console.log("str1 and str2 are:", str1, str2);
+
+//   // Normalize and sort the strings
+//   const sort1 = str1.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+//   const sort2 = str2.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+
+//   console.log("Sorted str1:", sort1);
+//   console.log("Sorted str2:", sort2);
+
+//   if (sort1 === sort2 && str1 !== str2) {
+//     document.getElementById("message").textContent = "The strings are anagrams (but not identical).";
+//   } else if (sort1 === sort2) {
+//     document.getElementById("message").textContent = "The strings are anagrams (including identical).";
+//   } else {
+//     document.getElementById("message").textContent = "The strings are NOT anagrams.";
+//   }
+// }
+
+
+//Compress the string
+
+function runFunction(){
+  console.log("Hello String");
+  const str1 = document.getElementById("str1").value;
+  console.log("String is: ", str1);
+  const inChar = str1.split('');
+  const compress ={}
+  console.log("total are: ", inChar)
+  for (let item in inChar){
+    compress(item) = (compress(item) || 0 ) + 1
+  }
+console.log("total are: ", compress)
+}
