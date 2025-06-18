d669d841e2265c0ca703d7e32d4cf4dda4a590b7
Satyam Srivastava <104303280+satya7041@users.noreply.github.com>
2025-06-18 11:25:06 +0000

    Arrays hof

---
+++ Arrays HOF/script.js
@@ -0,0 +1,17 @@
+// const myArray = ["Gagan", "satyam", "Ravi","Piyush"];
+
+// //this is HOF
+// // myArray.forEach(element => {
+    
+    // //     console.log("Arrays are: ", element)
+    // // });
+    // myArray.map((value)=> {
+        //     console.log("elements are: ", value)
+        // })
+
+
+  // print double of number
+  const myArray = [1,2,3,4,5,6];
+  let double = myArray.map((double)=> double * 2);
+
+  console.log(double)
